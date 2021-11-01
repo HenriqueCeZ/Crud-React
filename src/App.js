180a0a38bc960f5d15ddc7dd06aba3fc5af67cd5
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-11-01 05:28:28 -0300

    feat: add inputs, states and button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState('');
+  const [position, setPosition] = useState('');
+  const [wage, setWage]  = useState(0);
+
+  function displayInfo (){
+    console.log(name + age + country + position + wage)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="container">
+    <div className="form">
+    <label htmlFor="">Name: </label>
+    <input type="text" onChange={(event) =>{setName(event.target.value)}} />
+    <label htmlFor="">Age: </label>
+    <input type="number" onChange={(event) =>{setAge(event.target.value)}}  />
+    <label htmlFor="">Country: </label>
+    <input type="text" onChange={(event) =>{setCountry(event.target.value)}}  />
+    <label htmlFor="">Position: </label>
+    <input type="text" onChange={(event) =>{setPosition(event.target.value)}}  />
+    <label htmlFor="">Wage (year): </label>
+    <input type="number" onChange={(event) =>{setWage(event.target.value)}} />
+    <button > Add Employee</button>
     </div>
+  </div>
   );
 }
 
